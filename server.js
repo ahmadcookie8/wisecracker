149d11e1c54172ec16f7ded93374a9c60965a616
Huzaifa Sheikh <huzaifasheikh@hotmail.com>
2020-12-25 04:51:32 -0500

    added cors to server.js

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.use(bodyParser.json({
   limit: '50mb'
 }));
 
+//to bypass cors errors
+const cors = require("cors")
+
 app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
 app.use(express.static(__dirname + '/client/build'));
